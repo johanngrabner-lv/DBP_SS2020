66d178e3a953fdf080bb3f5c080b9b6d6798b96a
johanngrabner-lv <johann.grabner@lv.campus02.at>
2020-08-19 07:44:12 +0200

    First Draft

--- src/com/hans/Main.java
+++ src/com/hans/Main.java
@@ -3,6 +3,8 @@ package com.hans;
 public class Main {
 
     public static void main(String[] args) {
+        DbHelper myHelper=new DbHelper();
+        myHelper.OpenDatabase();
 	System.out.println("Hello Database");
     }
 }
