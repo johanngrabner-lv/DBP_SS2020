6605dd0fa2076251a57315a7bdc73eaca14514a4
johanngrabner-lv <johann.grabner@lv.campus02.at>
2020-08-19 13:40:27 +0200

    First Draft

--- src/com/hans/DbHelper.java
+++ src/com/hans/DbHelper.java
@@ -1,18 +1,27 @@
 package com.hans;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DbHelper {
     Connection con = null;
 
-    public void OpenDatabase(){
+    public void OpenDatabase() {
         try {
             con = DriverManager.getConnection("jdbc:sqlite:myFirstDatabase");
-        } catch (SQLException throwables) {
+            Statement statement = con.createStatement();
+            statement.setQueryTimeout(30);  // set timeout to 30 sec.
+            statement.executeUpdate("create table Kunden(i int)");
+            statement.executeUpdate("insert into Kunden values(2)");
+            ResultSet rs = statement.executeQuery("select * from kunden");
+            while (rs.next()) {
+                System.out.println(rs.getInt(1));
+            }
+            statement.executeUpdate("drop table kunden");
+
+        } catch (
+                SQLException throwables) {
             throwables.printStackTrace();
         }
     }
 }
+
