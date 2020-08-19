969ca82e64bda73ea9e534b6ac46a1e2a732da57
johanngrabner-lv <johann.grabner@lv.campus02.at>
2020-08-19 07:40:56 +0200

    First Draft

--- src/DbHelper.java
+++ src/DbHelper.java
@@ -1,9 +1,16 @@
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class DbHelper {
-    public void OpenDatabase(){
-        Connection con = null;
+    Connection con = null;
 
+    public void OpenDatabase(){
+        try {
+            con = DriverManager.getConnection("jdbc:sqlite:myFirstDatabase");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 }
