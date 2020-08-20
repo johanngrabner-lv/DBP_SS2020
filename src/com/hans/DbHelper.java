797226bfb4d8736e47baf7d733b93f5b110b369d
johanngrabner-lv <johann.grabner@lv.campus02.at>
2020-08-20 11:00:17 +0200

    First Draft

--- src/com/hans/DbHelper.java
+++ src/com/hans/DbHelper.java
@@ -6,10 +6,7 @@ leere Methode OpenDatabase
 in der Main Hello JDBC ausgeben
  */
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 /* JAR - Java Archive --- Interface, Classe
 https://github.com/johanngrabner-lv/DBP_SS2020/blob/master/sqlite-jdbc-3.30.1.jar
@@ -36,8 +33,41 @@ public class DbHelper {
             //Statment kapselt SQL-Anweisungen
             Statement stmt = con.createStatement();
 
+            stmt.executeUpdate("DROP TABLE Kunden");
             //Abschicken an die Datenbank
+            //CREATE, INSERT, UPDATE, DELETE --- executeUpdate
             stmt.executeUpdate(createTable);
+
+            stmt.executeUpdate("INSERT INTO Kunden VALUES('Aiste')");
+            stmt.executeUpdate("INSERT INTO Kunden VALUES('Emina')");
+            stmt.executeUpdate("INSERT INTO Kunden VALUES('Kertin')");
+
+            //SELECT -->  stmt.executeQuery()
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public void ReadKunden()
+    {
+        try {
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT rowid, vorname FROM  Kunden");
+            //rs Zeiger auf das "virtuelle Ergebniss"
+            //mit rs.Next() wird der Zeiger auf die nächste Zeile gesetzt
+            /*
+            rs.next(); //1. Zeile --> true, while eine Zeile vorhanden ist
+            rs.next(); //2. Zeile --> true
+            rs.next(); // 3. Zeile --> true
+            rs.next(); //am Ende der Tabelle --> false
+            */
+            while(rs.next()){
+                int rowid = rs.getInt("rowid");
+                String vorname = rs.getString("Vorname");
+                System.out.println("RowId " + rowid + " Vorname " + vorname);
+            }
+
 
         } catch (SQLException throwables) {
             throwables.printStackTrace();
--- src/com/hans/Main.java
+++ src/com/hans/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
         DbHelper myDbHelper=new DbHelper();
         myDbHelper.OpenDatabase();
+        myDbHelper.CreateKundenTable();
+        myDbHelper.ReadKunden();
 	    System.out.println("Hello JDBC");
     }
 }
