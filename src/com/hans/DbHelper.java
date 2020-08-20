2e81e4bc395209c15f5202af6db4d5e94fb3a713
johanngrabner-lv <johann.grabner@lv.campus02.at>
2020-08-20 10:12:47 +0200

    First Draft

--- src/com/hans/DbHelper.java
+++ src/com/hans/DbHelper.java
@@ -1,27 +1,16 @@
 package com.hans;
 
-import java.sql.*;
+
+/*1. neue Klasse - DbHelper
+leere Methode OpenDatabase
+in der Main Hello JDBC ausgeben
+ */
 
 public class DbHelper {
-    Connection con = null;
 
     public void OpenDatabase() {
-        try {
-            con = DriverManager.getConnection("jdbc:sqlite:myFirstDatabase");
-            Statement statement = con.createStatement();
-            statement.setQueryTimeout(30);  // set timeout to 30 sec.
-            statement.executeUpdate("create table Kunden(i int)");
-            statement.executeUpdate("insert into Kunden values(2)");
-            ResultSet rs = statement.executeQuery("select * from kunden");
-            while (rs.next()) {
-                System.out.println(rs.getInt(1));
-            }
-            statement.executeUpdate("drop table kunden");
 
-        } catch (
-                SQLException throwables) {
-            throwables.printStackTrace();
-        }
+
     }
 }
 
--- src/com/hans/Main.java
+++ src/com/hans/Main.java
@@ -3,8 +3,7 @@ package com.hans;
 public class Main {
 
     public static void main(String[] args) {
-        DbHelper myHelper=new DbHelper();
-        myHelper.OpenDatabase();
-	System.out.println("Hello Database");
+
+	System.out.println("Hello JDBC");
     }
 }
