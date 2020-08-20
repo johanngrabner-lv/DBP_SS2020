b529ef2e71fa889b7821f5b69e06e6df64078768
johanngrabner-lv <johann.grabner@lv.campus02.at>
2020-08-20 10:42:09 +0200

    First Draft

--- src/com/hans/DbHelper.java
+++ src/com/hans/DbHelper.java
@@ -9,6 +9,7 @@ in der Main Hello JDBC ausgeben
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /* JAR - Java Archive --- Interface, Classe
 https://github.com/johanngrabner-lv/DBP_SS2020/blob/master/sqlite-jdbc-3.30.1.jar
@@ -20,14 +21,27 @@ public class DbHelper {
     private Connection con =null;
 
     public void OpenDatabase() {
-        String connectionString="jdbc:sqlit:dbHans";
+        String connectionString="jdbc:sqlite:dbHans";
         try {
             con = DriverManager.getConnection(connectionString);
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
+    }
 
+    public void CreateKundenTable(){
+        String createTable="CREATE TABLE Kunden(Vorname varchar(20));";
 
+        try {
+            //Statment kapselt SQL-Anweisungen
+            Statement stmt = con.createStatement();
+
+            //Abschicken an die Datenbank
+            stmt.executeUpdate(createTable);
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 }
 
